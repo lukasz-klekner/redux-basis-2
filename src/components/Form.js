456e16a164c76ab49968bb0d220122e8977ea0c0
Lukasz Klekner <lukaszklekner@gmail.com>
2021-05-05 14:08:46 +0200

    created Form component and added it to the App.js file

---
+++ src/components/Form.js
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+
+const Form = ({ author = '', rate = 0, comment = '', id}) => {
+    const [ authorInput, setAuthorInput] = useState('');
+    const [ rateInput, setRateInput] = useState(0);
+    const [ commentInput, setCommentInput] = useState('');
+
+    const handleChangeAuthor = e => setAuthorInput(e.target.value)
+
+    const handleChangeRate = e => setRateInput(e.target.value)
+
+    const handleChangeComment = e => setCommentInput(e.target.value)
+
+    const handleOnSubmit = e => {
+        e.preventDefault();
+
+        const rateObject = {
+            author: authorInput,
+            rate: Number(rateInput),
+            comment: commentInput,
+            id,
+        }
+
+        id ? console.log('Edytuj komentarz') : console.log('Dodaj komentarz')
+    }
+
+    return (
+        <form onSubmit={handleOnSubmit}>
+            <p>
+                <label htmlFor="author">
+                    Autor
+                    <input
+                        type="text"
+                        id="author"
+                        value={authorInput}
+                        onChange={handleChangeAuthor}
+                    />
+                </label>
+            </p>
+            <p>
+                <label htmlFor="grade">
+                    Ocena
+                    <input
+                        type="number"
+                        id="grade"
+                        value={rateInput}
+                        onChange={handleChangeRate}
+                    />
+                </label>
+            </p>
+            <p>
+                <label htmlFor="comment">
+                    Komentarz
+                    <input
+                        type="text"
+                        id="comment"
+                        value={commentInput}
+                        onChange={handleChangeComment}
+                    />
+                </label>
+            </p>
+            <button type="submit">{id ? 'Edytuj' : 'Dodaj'}</button>
+        </form>
+    )
+}
+
+export default Form;
